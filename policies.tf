ff7dc15f2e2da76452209afe8c92aa531bf1c9e0
Mustafa Mehdi <mustafamehdi002@gmail.com>
2026-02-17 19:49:43 +0530

    Added QA custom IAM policy with restricted EC2 access

--- policies.tf
+++ policies.tf
@@ -51,3 +51,54 @@ resource "aws_iam_group_policy_attachment" "dev_attach" {
   policy_arn = aws_iam_policy.developer_policy.arn
 }
 
+data "aws_iam_policy_document" "qa_policy" {
+
+  statement {
+    actions = [
+      "ec2:Describe*"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "s3:*"
+    ]
+    resources = [
+      aws_s3_bucket.qa_bucket.arn,
+      "${aws_s3_bucket.qa_bucket.arn}/*"
+    ]
+  }
+
+  statement {
+    actions = [
+      "logs:Describe*",
+      "logs:Get*",
+      "logs:List*"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Deny"
+    actions = [
+      "ec2:StartInstances",
+      "ec2:StopInstances",
+      "ec2:TerminateInstances",
+      "iam:*"
+    ]
+    resources = ["*"]
+  }
+}
+
+
+resource "aws_iam_policy" "qa_policy" {
+  name   = "QACustomPolicy"
+  policy = data.aws_iam_policy_document.qa_policy.json
+}
+
+resource "aws_iam_group_policy_attachment" "qa_attach" {
+  group      = aws_iam_group.qa.name
+  policy_arn = aws_iam_policy.qa_policy.arn
+}
+
